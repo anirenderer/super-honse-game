1c70371d6b92c8b84db0a0628db8e51bc6500411
anirenderer <anirenderrr@gmail.com>
2025-12-09 19:18:12 -0500

    package stuff

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // Vite base should match your GitHub Pages repo path.
 // This makes built assets use absolute paths like /super-honse-game/
 export default defineConfig({
-  base: '/super-honse-game/'
+  base: '/super-honse-game/',
+  plugins: [vue()]
 })
